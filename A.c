b5b974d9ad0f95b6df8d1a5e66d1a95885cbb44a
UjinJung <ujin.jung9@gmail.com>
2016-11-25 17:56:43 -0800

    2

--- A.c
+++ A.c
@@ -64,6 +64,19 @@
 
 //#include <stdio.h>
 //#include <stdlib.h>
+#include <string.h>
+#include <Math.h>
+#include <Servo.h> 
+// Speed defines
+#define maxForwardSpeed 120 // Max speed we want moving forward
+#define maxBackwardSpeed -120 // Max reverse speed
+#define Stop 90
+
+// Various time delays used for driving and servo
+#define moveDelay 950
+ 
+int LEFT_SERVO = 5;
+int RIGHT_SERVO = 6;
 
 int WIDTH = 0;
 
@@ -71,6 +84,7 @@ typedef struct Node{
 	int f = 0;
 	int g = 0;
 	char state = 0; // none, open, close (0, 1, 2)state
+	int camefrom = 0;
 }Node;
 
 
@@ -111,34 +125,50 @@ typedef struct Node{
 
 
 set{
+	motor_left.attach(LEFT_SERVO);
+	motor_right.attach(RIGHT_SERVO);
+
 	//방크기 입력받고
-	
+	Serial.begin(115200);
 }
 
 
 loop{
-	//신호 기다림
-	if(Serial.available()) {
-		String direct = Serial.readStringUntil('\n');
-		
-	}
+	int start, goal;
+	int arrayWidth, maxCost;
+	int* closeSet;
 	
+	//신호 기다림
+	// signal form : start goal arrayWidth maxCost closeSet,closeSet,closeSet
+	if(Serial.available()) {
+		String readString  = Serial.readStringUntil('\n');
+		char* separateString = strtok(readString, " ");
+		start = separateString[0];
+		goal = separateString[1];
+		arrayWidth = separateString[2];
+		maxCost = separateString[3];
+		
+		//close Set
+		closeSet = strtok(separateString[4], ",");
+	}
+
+/*	
 	while(signal){
 	
 
 	}
 
-	//자동 수동?
-	if(auto){
+*/	//자동 수동?
+	//if(auto){
 		//각각에 signal 넣고.
 		//Start / Goal / closeSet / 바라볼 위치
 			
 		//path 따라 움직이고
 		moving(start, goal, arrayWidth, maxCost, closeSet);
 
-	}else if(passive){
+	//}else if(passive){
 		//
-	}
+//	}
 	
 }
 
@@ -148,11 +178,13 @@ void moving(int start, int goal, int arrayWidth, int maxCost, bool* closeSet){
 	//int past, future;  // past, future Index
 	int Calculate_tmp_x, Calculate_tmp_y; // temp_mode
 	char pathMode, currentMode;
-
+	
+	//init current, path
 	current = start;
 	path = (int *)malloc(sizeof(int)*maxcost);
 
 	path = Astar(start, goal, arrayWidth, maxCost, closeSet);
+	
 	
 	while(current == goal){
 		unsigned int uS = sonar.ping_cm();
@@ -168,9 +200,10 @@ void moving(int start, int goal, int arrayWidth, int maxCost, bool* closeSet){
 			detect_obstacle = ((path[current + 1]/arrayWidth) + (Calculate_tmp_x*2))*arrayWidth
 								+ ((path[current + 1] % arrayWidth) + (Calculate_tmp_y*2));
 			closeSet[detect_obstacle] = TRUE;
+			
+			moving(current, goal, arrayWidth, maxCost, closeSet); 
 		}
 	
-
 		Calculate_tmp_x = (path[current + 1]/arrayWidth) - (path[current]/arrayWidth);
 		Calculate_tmp_y = (path[current + 1]%arrayWidth) - (path[current]%arrayWidth);
 		currentMode = abs(Calculate_tmp_x)*(1-Calculate_tmp_x) + abs(Calculate_tmp_y)*(2 + Calculate_tmp_y);
@@ -208,22 +241,6 @@ void moving(int start, int goal, int arrayWidth, int maxCost, bool* closeSet){
 
 	}
 	
-	
-	/* 구현 할 거
-	
-	unsigned int uS = sonar.ping_cm();
-
-	if(uS <= 25){
-		current
-	}	
-	obstacle detect process
-	
-	if(obstacle){
-		current 를 closeSet으로
-		moving (current, goal, arrayWidth, maxCost, closeSet);
-	}	
-	*/
-
 	return;
 }
 
@@ -257,6 +274,8 @@ int* Astar(int start, int goal, int arrayWidth, int maxCost, int* para_closeSet)
 	openSet[openSetNum] = start;
 	openSetNum++;
 	memset(closeSet, 0, sizeof( int ) * maxCost ); 	
+	
+	camefrom[start] = 0;
 
 	node[start].f = h(start, goal, arrayWidth);
 	node[start].g = 0;
@@ -267,7 +286,7 @@ int* Astar(int start, int goal, int arrayWidth, int maxCost, int* para_closeSet)
 		
 		//End
 		if(currentNode == goal)
-			return build_Path(camefrom, currentNode);
+			return build_Path(camefrom, currentNode, node[goal].f, maxCost);
 
 		//remove currentNode in openSet
 		openSet[openSetNum] = 0;
@@ -293,8 +312,9 @@ int* Astar(int start, int goal, int arrayWidth, int maxCost, int* para_closeSet)
 				case 3:	// forward
 					neighbor = currentNode + arrayWidth;
 					break;
+				//들어올 일 없음.
 				default:
-					return fail; //fail
+					continue; //fail
 			}
 			//
 			if(node[neighbor].state == 2) // neighbor is closeSet
@@ -327,4 +347,20 @@ int h(int start, int goal, int arrayWidth){
 	y_dis = abs((start%arrayWidth) - (goal%arrayWidth));
 
 	return (x_dis + y_dis);
+}
+
+int* build_Path(int* camefrom, int currentNode, int fScore, int maxCost){
+	int currentNode_t;	
+	int temp;
+	int buildingPath[fScore];
+	int index = 0;
+	
+	currentNode_t = currentNode;
+
+	while(camefrom[currentNode_t] != 0){
+		buildingPath[fScore - index] = camefrom[currentNode_t];
+		currentNode_t = camefrom[currentNode_t];
+		index++;
+	}
+	return buildingPath;	
 }
